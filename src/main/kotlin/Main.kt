5341cb58ac4905b4c392044ff933d806235a0fc8
Etern <makinoharataka@gmail.com>
2024-09-18 15:45:16 +0100

    Why i am im still here just to suffer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 package ie.setu
 
-import javax.print.event.PrintJobEvent
+import kotlin.math.round
+
 val fName = "Joe"
 val lName = "Soap"
-val geneder = "(m)"
+val gender = 'm'
 val employeeId = 6143
 val gSalary = 67543.21
 val payePercent = 38.5
@@ -11,6 +12,8 @@ val prsiPercent = 5.2
 val bonus = 1450.50
 val cycle = 54.33
 fun main(args: Array<String>) {
+    println("Payslip Printer")
+    println(getFullName())
     paySlipPrinter()
 }
 fun paySlipPrinter() {
@@ -23,27 +26,36 @@ fun paySlipPrinter() {
     val tdecu= (paye+prsi+cycle)
     val npay = (grSalary-tdecu)
 
-    println("---------------------------------------------------------------------------")
-    println("|                          Monthly Payslip                                |")
-    println("|--------------------------------------------------------------------------")
-    println("|                                                                         |")
-    println("|  Employee Name: " +  fName + " " + lName + geneder + "                Employee ID: " + employeeId + "            |")
-    println("|                                                                         |")
-    println("--------------------------------------------------------------------------|")
-    println("|                                                                         |")
-    println("|  Payment Details:                            Deduction details          |")
-    println("|                                                                         |")
-    println("--------------------------------------------------------------------------|")
-    println("  Salary: " +mSalary+ "                          PayE: "+paye +"        |")
-    println("  Bonus: " + bonus + "                          Prsi: "+prsi+"       |")
-    println("|                                                                         |")
-    println("--------------------------------------------------------------------------|")
-    println("|                                                                         |")
-    println("| Gross:" +grSalary+ "                         Total Desuctions:"+tdecu+"                                     |")
-    println("|                                                                         |")
-    println("--------------------------------------------------------------------------|")
-    println("|                                                                         |")
-    println("|                    Net Pay: "+npay+"                                         |")
-    println("|                                                                         |")
-    println("--------------------------------------------------------------------------|")
+    println ("""
+    |_________________________________________________________________________
+    |                          Monthly Payslip                                
+    |_______________________________________________________________________  
+    |  Employee Name: ${fName}(${gender.uppercase()})            Employee ID: ${employeeId} 
+    |                                                                         
+    |-------------------------------------------------------------------------
+    |
+    |
+    |                                                                         
+    |  Payment Details:                            Deduction details        
+    |                                                                         
+    |-------------------------------------------------------------------------
+    |Salary: ${roundTwoDecimals(mSalary)}                         PayE: ${roundTwoDecimals(paye)}       
+    | Bonus: ${bonus}                       Prsi: ${roundTwoDecimals(paye)}      
+    |                                                                        
+    |-------------------------------------------------------------------------
+    |                                                                         
+    | Gross:" ${roundTwoDecimals(grSalary)}                           Total Desuctions:${roundTwoDecimals(tdecu)}                                      
+    |                                                                         
+    |-----------------------------------------------------------------------
+    |                                                                         
+    |                    Net Pay: ${roundTwoDecimals(npay)}                                  
+    |  |                                                                     
+    |------------------------------------------------------------------------
+""".trimMargin("|"))
 }
+fun getFullName() = when (gender){
+    'm', 'M' -> "Mr. $fName $lName"
+    'f', 'F' -> "Ms. $fName $lName"
+    else -> "$fName $lName"
+}
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
